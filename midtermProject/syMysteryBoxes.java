01c65d60c7a96c7a46725d9b853fb6bbc24d2a93
Sa-gg <108111067+Sa-gg@users.noreply.github.com>
2023-11-07 21:00:46 +0800

    Add files via upload

---
+++ midtermProject/syMysteryBoxes.java
@@ -0,0 +1,57 @@
+package midtermProject;
+import java.util.*;
+
+public class syMysteryBoxes {
+	static Scanner scan = new Scanner(System.in);
+	static Random random = new Random();
+	public static void main(String[] args) {
+		
+		System.out.println("List of Mystery Boxes: \n 1. Mystery Box A (Php 8,000) \n 2. Mystery Box B (Php 5,000)");
+		System.out.print("Enter 1 or 2 to choose the type of mystery boxes: ");
+		int mysteryBoxType = scan.nextInt();
+		
+		if (mysteryBoxType != 1 && mysteryBoxType != 2) {
+			System.err.println("Invalid Entry");
+		} else {
+			int rdm = random.nextInt(10);
+			String prize = "";
+			double mysteryBoxPrice = 0;
+			double price = 0;
+			
+			if (mysteryBoxType == 1) {
+				mysteryBoxPrice = 8000;
+				
+				switch (rdm) {
+				case 0: prize = "Diamond Necklace"; price = 20000; break;
+				case 1: prize = "Watch"; price = 15000; break;
+				case 2: prize = "Wine"; price = 8000; break;
+				case 3: prize = "Bag"; price = 10000; break;
+				case 4: prize = "Shoes"; price = 9000; break;
+				case 5: prize = "Ring"; price = 15000; break;
+				case 6: prize = "Clothes"; price = 10000; break;
+				case 7: prize = "5 Star Hotel Accomodation"; price = 18000; break;
+				case 8: prize = "Perfume"; price = 11000; break;
+				case 9: prize = "Make Up"; price = 17000; break;
+				}
+			}  else if (mysteryBoxType == 2) {
+				mysteryBoxPrice = 5000;
+
+				switch (rdm) {
+				case 0: prize = "Mobile Phone"; price = 10000; break;
+				case 1: prize = "Gaming Credits"; price = 9000; break;
+				case 2: prize = "Headset"; price = 5000; break;
+				case 3: prize = "Speaker"; price = 8000; break;
+				case 4: prize = "Keyboard & Mouse Set"; price = 5500; break;
+				case 5: prize = "Microphone"; price = 6000; break;
+				case 6: prize = "Graphic Tablet"; price = 9000; break;
+				case 7: prize = "Monitor"; price = 6000; break;
+				case 8: prize = "Gaming Chair"; price = 7000; break;
+				case 9: prize = "SSD Hard Disk"; price = 9000;
+				}
+				
+			}	
+			System.out.println("You got a " + prize.toUpperCase());
+			System.out.println("Profit: Php "+ (price - mysteryBoxPrice)); 
+		}
+	} 
+}
---
+++ midtermProject/syStudentMealPlan.java
@@ -0,0 +1,116 @@
+package midtermProject;
+import java.util.*;
+
+public class syStudentMealPlan {
+	static Scanner scan = new Scanner(System.in);
+	static Random random = new Random();
+			
+	public static void main(String[] args) {
+
+		System.out.print("Enter Queue Number: ");
+		int qNum = scan.nextInt();
+		
+//		MA GENRERATE NISA RANDOM NUMBERS (0-2) 
+		int randomPrize = random.nextInt(3); 
+		int randomCashGift = random.nextInt(3);
+		
+		String prize = ""; 
+		double cashGift = 0;
+		
+		switch (randomPrize) {
+		case 0: prize = "50% discounts on all meals"; break;
+		case 1: prize = "Buy 1 take 1 on all meals. "; break;
+		case 2: switch (randomCashGift) {
+			case 0: cashGift = 300; break;
+			case 1: cashGift = 500; break;
+			case 2: cashGift = 1000; break;
+			}
+			prize = "20% discounts on all meals + a " + cashGift + " cash gift."; break;
+		}
+		
+		if (qNum <= 100) {
+			System.out.print("Congratulations! You won " + prize);
+		} else {
+			System.out.print("Sorry you are not eligible for the raffle.");
+		}
+		
+//		MENU LIST
+		System.out.println("Please Choose Your Order\n");
+		System.out.println("============================================================================================================");
+		System.out.println(String.format("%56s", "MENU LIST"));
+		System.out.println("============================================================================================================");
+		System.out.println("Combo Meal Package 1: Classic Burgers and Fries                                                 Php 109.99");
+		System.out.println("  *Cheeseburgers, French Fries, Medium Soft Drinks");
+		System.out.println("------------------------------------------------------------------------------------------------------------");
+		System.out.println("Combo Meal Package 2: Chicken Lovers                                                            Php 240.99");
+		System.out.println("  *Crispy Chicken Sandwiches, Grilled Chicken Wraps, Chicken Nuggets, Small Vanilla Milkshake");
+		System.out.println("------------------------------------------------------------------------------------------------------------");
+		System.out.println("Combo Meal Package 3: Vegetarian Delight                                                        Php 130.99");
+		System.out.println("  *Veggie Burgers, Garden Salads, Sweet Potato Fries, Bottled Waters");
+		System.out.println("------------------------------------------------------------------------------------------------------------");
+		System.out.println("Combo Meal Package 4: Tex-Mex Fiesta                                                            Php 135.99");
+		System.out.println("  *Beef Burritos, Chicken Quesadillas, Nachos with Cheese, Medium Soft Drinks (Lemon-Lime Soda)");
+		System.out.println("------------------------------------------------------------------------------------------------------------");
+		System.out.println("Combo Meal Package 5: Breakfast for Lunch                                                       Php 150.99");
+		System.out.println("  *Breakfast Biscuits (e.g., sausage, egg, and cheese), Hash Browns, Bottled Orange Juices");
+		System.out.println("------------------------------------------------------------------------------------------------------------");
+		System.out.println("Combo Meal Package 6: Spicy Selection                                                           Php 200.99");
+		System.out.println("  *Spicy Chicken Sandwiches, Jalapeño Poppers, Medium Soft Drinks (Cola)");
+		System.out.println("------------------------------------------------------------------------------------------------------------");
+		System.out.println("Combo Meal Package 7: Asian Fusion                                                              Php 189.99");
+		System.out.println("  *Teriyaki Chicken Rice Bowls, Vegetable Spring Rolls, Bottled Green Teas");
+		System.out.println("------------------------------------------------------------------------------------------------------------");
+		System.out.println("Combo Meal Package 8: Fish and Chips                                                            Php 170.99");
+		System.out.println("  *Fish Sandwiches, Onion Rings, Medium Soft Drinks (Cola)");
+		System.out.println("------------------------------------------------------------------------------------------------------------");
+		System.out.println("Combo Meal Package 9: Wrap It Up                                                                Php 156.99");
+		System.out.println("  *Chicken Caesar Wraps, Veggie Wraps, Sweet Potato Fries, Bottled Waters");
+		System.out.println("------------------------------------------------------------------------------------------------------------");
+		System.out.println("Combo Meal Package 10: Pizza Party                                                              Php 250.99");
+		System.out.println("  *Large Pepperoni Pizzas, Large Cheese Pizzas, Garlic Knots, Cans of Soda (various flavors)");
+		System.out.println("------------------------------------------------------------------------------------------------------------\n");
+		
+		System.out.print("Enter Your Order: ");
+		int order = scan.nextInt();
+		System.out.print("Quantity: ");
+		int quantity = scan.nextInt();
+		
+		if ((order <= 0 && order >= 11) || (quantity <= 0)) {
+			System.err.println("Invalid Entry");
+		} else {
+			double total = 0, remainingCashGift = 0;
+			
+			switch (order) {
+			case 1: total = quantity * 109.99; break;
+			case 2: total = quantity * 240.99; break;
+			case 3: total = quantity * 130.99; break;
+			case 4: total = quantity * 135.99; break;
+			case 5: total = quantity * 150.99; break;
+			case 6: total = quantity * 200.99; break;
+			case 7: total = quantity * 189.99; break;
+			case 8: total = quantity * 170.99; break;
+			case 9: total = quantity * 156.99; break;
+			case 10: total = quantity * 250.99; break;
+			}
+			
+			if (qNum <= 100) {
+				switch (randomPrize) {
+				case 0: total *= .50; break;
+				case 1: quantity *= 2; break;
+				case 2: total = (total * .80) - cashGift;
+					if (total < 0) {
+						remainingCashGift = total * (-1);
+						total = 0;
+					}		
+				}
+			}
+			
+			System.out.println("Number of Meals to be Served: " + quantity);
+			System.out.println("Total Amount to Pay: " + "Php " + String.format("%.2f", total));
+			if (randomPrize == 2) {
+				System.out.println("Total Remaining Cash Gift: Php " + String.format("%.2f", remainingCashGift));
+			}
+		}
+
+	}	
+}
