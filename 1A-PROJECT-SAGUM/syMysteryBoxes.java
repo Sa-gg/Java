e777a80d33e997e693e4cf367a63f42f2b56949b
Sa-gg <108111067+Sa-gg@users.noreply.github.com>
2023-11-11 13:34:13 +0800

    Update syMysteryBoxes.java

--- 1A-PROJECT-SAGUM/syMysteryBoxes.java
+++ 1A-PROJECT-SAGUM/syMysteryBoxes.java
@@ -32,7 +32,7 @@ public class syMysteryBoxes {
 				case 8: prize = "Perfume"; price = 11000; break;
 				case 9: prize = "Make Up"; price = 17000; break;
 				}
-				max = (int) (mysteryBoxPrice / price);
+				max = (int) (50000 / price);
 				
 			}  else if (mysteryBoxType == 2) {
 				
@@ -49,7 +49,7 @@ public class syMysteryBoxes {
 				case 8: prize = "Gaming Chair"; price = 7000; break;
 				case 9: prize = "SSD Hard Disk"; price = 9000;
 				}
-				max = (int) (mysteryBoxPrice / price);
+				max = (int) (30000 / price);
 				
 		} else {
 			System.err.println("Sorry, you have entered an invalid number");
